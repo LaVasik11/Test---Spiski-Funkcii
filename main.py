b92a75f52615dbe96138fe2a421e97090635b8ad
lavasik11 <128045646+LaVasik11@users.noreply.github.com>
2024-09-17 19:27:12 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,88 +1,111 @@
 import random
 import time
 from random import *
-imena = ["Ксения", "Варвара", "Даниил", "Тамара", "Михаил", "Денис", "Анастасия", "Николай", "Ариана", "Макар",
+
+
+names = ["Ксения", "Варвара", "Даниил", "Тамара", "Михаил", "Денис", "Анастасия", "Николай", "Ариана", "Макар",
          "Василиса", "Ольга", "Иван", "Георгий", "Мила", "Ника", "Александра", "Фёдор", "Марина", "Мирон", "Александр",
          "Дарья", "Алина", "Анна", "Милана", "Арсений", "Мария", "Никита", "Тимофей", "Лейла", "Ярослав", "Роман",
          "Владимир", "Дмитрий", "Артём", "Роберт", "Екатерина", "Савелий", "Григорий", "Филипп","Матвей", "Ульяна",
          "Вячеслав", "Наталья", "Мирослав", "Виктория", "Алексей", "Глеб", "Марк", "Всеволод"]
-ludi = ["Артур","Кирилл","Стас"]
-rost = [180,174,186]
-while True:
-    def menu():
-        print("1 - Посмотреть список людей и из рост \n2 - Добавить человека и его рост в список "
-              "\n3 - Удалить человека из списка \n4 - Отсортировать лбдей в алфавитном порядке "
-              "\n5 - Показать самого высокого и низкого человека \n6 - Показать людей со средним ростом "
-              "\n7 - Добавить имена в список")
-        a = int(input())
-        if a == 1:
-            show()
-        if a == 2:
-            add()
-        elif a == 3:
-            delete()
-        elif a == 4:
-            sort()
-        elif a == 5:
-            min_max()
-        elif a == 6:
-            sred()
-        elif a == 7:
-            inimesed()
-    def show():
-        print(ludi)
-        print(rost)
-    def add():
-        print("Введите имя человека которого хотите добавить")
-        add=input()
-        ludi.append(add)
-        print("Введите рост этого человека")
-        add_rost=input()
-        rost.append(add_rost)
-        return ludi,rost
-    def delete():
-        print("Введите имя человека которого хотите удалить")
-        name=input()
-        for i in range(len(ludi)):
-            if ludi[i] == name:
-                rost.pop(i)
-                ludi.remove(ludi[i])
-                return i
-        return ludi, rost
-    def sort():
-        print("....Запуск....")
-        s = "█"
-        for i in range(101):
-            time.sleep(0.05)
-            print("\r", "Загрузка", i * s, str(i), '%', end="")
-        print("\nЛюди отсортированы")
-        abi_p = 0
-        abi_i = ""
-        n = len(rost)
-        for i in range(0, n - 1):
-            for j in range(i + 1, n):
-                if ludi[i] > ludi[j]:
-                    abi_p = ludi[i]
-                    ludi[i] = ludi[j]
-                    ludi[j] = abi_p
-                    abi_i = rost[i]
-                    rost[i] = rost[j]
-                    rost[j] = abi_i
-        return ludi, rost
-    def min_max():
-        for i in range (len(rost)):
-            if rost[i] == min(rost):
-                print(min(rost)," ",ludi[i])
-        for i in range (len(rost)):
-            if rost[i] == max(rost):
-                print(max(rost), " ", ludi[i])
-    def sred():
-        print("Средний рост трёх первых человек",(ludi[0],ludi[1],ludi[2]), "равен",(rost[0]+rost[1]+rost[2])/3)
-    def inimesed():
-        print("Сколько имён вы хотите добавить в список? 1 - 50")
-        kol_vo=int(input())
-        for i in range (kol_vo):
-            ludi.append(imena[randint(0,49)])
-            a = randint(130, 200)
-            rost.append(a)
+people = ["Артур", "Кирилл", "Стас"]
+height = [180, 174, 186]
+
+
+def menu():
+    possibilities = {
+        1: show,
+        2: add,
+        3: delete,
+        4: sort,
+        5: min_max,
+        6: sred,
+        7: inimesed,
+    }
+
+    print("1 - Посмотреть список людей и из рост\n"
+          "2 - Добавить человека и его рост в список\n"
+          "3 - Удалить человека из списка\n"
+          "4 - Отсортировать лбдей в алфавитном порядке\n"
+          "5 - Показать самого высокого и низкого человека\n"
+          "6 - Показать людей со средним ростом\n"
+          "7 - Добавить имена в список\n")
+
+    action = int(input())
+    possibilities[action]()
+
+
+def show():
+    print(people)
+    print(height)
+
+
+def add():
+    print("Введите имя человека которого хотите добавить")
+    name = input()
+    people.append(name)
+    
+    print("Введите рост этого человека")
+    height_person = int(input())
+    height.append(height_person)
+    
+    return people, height
+
+
+def delete():
+    print("Введите имя человека которого хотите удалить")
+    name = input()
+    for i in range(len(people)):
+        if people[i] == name:
+            height.pop(i)
+            people.remove(people[i])
+            return i
+    return people, height
+
+
+def sort():
+    print("....Запуск....")
+    s = "█"
+    for i in range(101):
+        time.sleep(0.05)
+        print("\r", "Загрузка", i * s, str(i), '%', end="")
+    print("\nЛюди отсортированы")
+    abi_p = 0
+    abi_i = ""
+    n = len(height)
+    for i in range(0, n - 1):
+        for j in range(i + 1, n):
+            if people[i] > people[j]:
+                abi_p = people[i]
+                people[i] = people[j]
+                people[j] = abi_p
+                abi_i = height[i]
+                height[i] = height[j]
+                height[j] = abi_i
+    return people, height
+
+
+def min_max():
+    for i in range (len(height)):
+        if height[i] == min(height):
+            print(min(height), " ", people[i])
+    for i in range (len(height)):
+        if height[i] == max(height):
+            print(max(height), " ", people[i])
+
+
+def sred():
+    print("Средний рост трёх первых человек", (people[0], people[1], people[2]), "равен", (height[0] + height[1] + height[2]) / 3)
+
+
+def inimesed():
+    print("Сколько имён вы хотите добавить в список? 1 - 50")
+    kol_vo=int(input())
+    for i in range (kol_vo):
+        people.append(names[randint(0,49)])
+        a = randint(130, 200)
+        height.append(a)
+
+
+if __name__ == '__main__':
     menu()
